8a6b46440fa2d24781e8258f276507a4f37e38ad
river-flows-in-ryu <rgr65066384@gmail.com>
2025-07-21 17:43:35 +0900

    custom header 추가

---
+++ components/customHeader.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useRouter } from "expo-router";
+import { TouchableOpacity, View, Text } from "react-native";
+
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+export default function CustomHeader({ title }: { title: string }) {
+  const router = useRouter();
+  return (
+    <View className="flex-row items-center p-4 bg-white ">
+      <TouchableOpacity onPress={() => router.back()}>
+        <AntDesign name="arrowleft" size={24} color="black"></AntDesign>
+      </TouchableOpacity>
+      <Text className="ml-5 text-xl font-semibold">{title}</Text>
+    </View>
+  );
+}
