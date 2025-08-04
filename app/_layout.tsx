fb4cffa4e62d6eda4cf26d3dfbce2839019b979b
river-flows-in-ryu <rgr65066384@gmail.com>
2025-08-04 16:29:11 +0900

    레이아웃 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,22 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Slot } from "expo-router";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+import { Platform } from "react-native";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   if (!loaded) {
@@ -18,12 +25,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <SafeAreaProvider>
+        <SafeAreaView className="flex-1 bg-white">
+          <Slot />
+        </SafeAreaView>
+        <StatusBar style={Platform.OS === "ios" ? "dark" : "auto"} />
+      </SafeAreaProvider>
     </ThemeProvider>
   );
 }
